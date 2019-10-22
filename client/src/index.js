d2f72664f67cb7df9945ad991d7643f54fcfbc2a
Kenya <kenya55@msn.com>
2019-10-22 14:15:06 -0400

    created dashboard, installed packages, created components folder

--- client/src/index.js
+++ client/src/index.js
@@ -2,13 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import Dashboard from "./components/dashboard"
+
 import registerServiceWorker from "./registerServiceWorker";
 import { Route, Switch } from "react-router-dom";
 import { BrowserRouter } from "react-router-dom";
 ReactDOM.render(
   <BrowserRouter>
     <Switch>
-      <Route path="/" component={App} />
+      <Route exact path="/" component={App} />
+      <Route exact path="/dashboard" component={Dashboard}/>
     </Switch>
   </BrowserRouter>,
   document.getElementById("root")
