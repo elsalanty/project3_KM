01891d48e6a7d86a3b971f8c327132202287cae9
elsalanty <elsalanty@gmail.com>
2019-10-22 09:13:33 -0400

    added react and google sign in button to app.js

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,14 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
-
-ReactDOM.render(<App />, document.getElementById("root"));
+import { Route, Switch } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
+ReactDOM.render(
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" component={App} />
+    </Switch>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
 registerServiceWorker();
