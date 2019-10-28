af2c58670ba9e7341b4622b43f9c0481e68ee43b
Kenya <kenya55@msn.com>
2019-10-28 12:50:28 -0400

    added new template for app, set up new module and quiz pages, moved app.js file for google login to new template folder

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import "./App.css";
 
 class App extends Component {
