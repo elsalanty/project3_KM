b7f4078efdeeb29ab3cad24786b1b796728536a1
Kenya <kenya55@msn.com>
2019-11-03 13:48:55 -0500

    added quizes and modules inside client folder

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,13 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import welcome from "./pages/welcome";
 import dashboard from "./pages/dashboard";
 import courses from "./pages/courses";
-import quiz from "./pages/quiz";
+import modules from './pages/Modules';
+import quiz from "./pages/Quizes";
 //import login2 from "./pages/login2";
 //import Navbar from "./components/Navbar";
 //import Footer from "./components/Footer";
 //import Wrapper from "./components/Wrapper";
+
 
 function App() {
   return (
@@ -17,6 +19,7 @@ function App() {
         <Route exact path="/login" component={welcome} />
         <Route exact path="/dashboard" component={dashboard} />
         <Route exact path="/courses" component={courses} />
+        <Route exact path="/modules" component={modules} />
         <Route exact path="/quiz" component={quiz} />
       </div>
     </Router>
--- client/src/index.js
+++ client/src/index.js
@@ -3,14 +3,27 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import dashboard from "./pages/dashboard";
+import Modules from './pages/Modules';
+import Quizes from './pages/Quizes';
 
 import registerServiceWorker from "./registerServiceWorker";
 import { Route, Switch } from "react-router-dom";
 import { BrowserRouter } from "react-router-dom";
+import AdminLayout from "./layouts/Admin";
+
+import "./assets/css/bootstrap.min.css";
+import "./assets/css/animate.min.css";
+import "./assets/sass/light-bootstrap-dashboard-react.scss";
+import "./assets/css/demo.css";
+import "./assets/css/pe-icon-7-stroke.css";
 ReactDOM.render(
   <BrowserRouter>
     <Switch>
       <Route exact path="/" component={App} />
+      {/* <Route path="/admin" render={props => <AdminLayout {...props} />} /> */}
+     
+      <Route exact path="/modules" component={Modules} />
+        <Route exact path="/quiz" component={Quizes} />
       <Route exact path="/dashboard" component={dashboard} />
     </Switch>
   </BrowserRouter>,
