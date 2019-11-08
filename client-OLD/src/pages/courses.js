cb0eca408940ab5f8d626fc7e6f3dcf1897b6baf
elsalanty <elsalanty@gmail.com>
2019-11-08 16:11:18 -0500

    deleted extra files

--- client-OLD/src/pages/courses.js
+++
@@ -1,7 +0,0 @@
-import React, { Component } from "react";
-
-export default class Courses extends React.Component {
-  render() {
-    return <h1>My Course Page</h1>;
-  }
-}
--- server/routes/api/quiz.js
+++
@@ -1,6 +0,0 @@
-const QuizController = require('../controller/quiz_controller');
-
-module.exports = app =>{
-
-    app.post('/api/quiz',QuizController.saveQuiz)
-}
