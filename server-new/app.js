e9f60df3f05ef1d71cc956ac275cea8a169f28a6
Kenya <kenya55@msn.com>
2019-11-08 12:04:59 -0500

    created build and added to package json

--- server-new/app.js
+++ server-new/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const routes= require('./routes/routes');
 const mongoose = require('mongoose');
+const path = require('path');
 
 const app = express();
 
